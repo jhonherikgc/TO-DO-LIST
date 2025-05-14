c0b043616fb50db978e282a85f4bdddb6f86d71f
JhonHerikgc <jhonherik006@gmail.com>
2025-05-14 15:26:46 +0100

    typing-funcionando

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,37 @@
-let typed = new Typed (".auto-type", {
-    strings: ["Tarefas", "Compras", "Lembretes"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    looped : true,
-})
+  const textos = [
+    "Tarefas",
+    "Compras",
+    "Lembretes",
+    "Tarefas do dia",
+    "Metas do dia"
+  ];
+
+  let i = 0;      
+  let j = 0;      
+  let apagando = false;
+  const elemento = document.getElementById("auto-type");
+
+  function loop() {
+    const frase = textos[i];
+
+    if (!apagando) {
+      elemento.textContent = frase.substring(0, j + 1);
+      j++;
+      if (j === frase.length) {
+        apagando = true;
+        setTimeout(loop, 1500); 
+        return;
+      }
+    } else {
+      elemento.textContent = frase.substring(0, j - 1);
+      j--;
+      if (j === 0) {
+        apagando = false;
+        i = (i + 1) % textos.length;
+      }
+    }
+
+    setTimeout(loop, apagando ? 50 : 100);
+  }
+
+  loop();
